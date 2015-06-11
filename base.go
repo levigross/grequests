b126dd6d4cbb6fd4ec2d33dea68568c7c69a2b6c
Levi Gross <levi@levigross.com>
2015-06-11 12:40:33 -0400

    Init
    Signed-off-by: Levi Gross <levi@levigross.com>

---
+++ base.go
@@ -0,0 +1,74 @@
+package grequests
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+// RequestOptions is the location that of where the data
+type RequestOptions struct {
+	// Data is a map of key values that will eventually convert into the query string of a GET request or the
+	// body of a POST request. Items can be passed in as an interface (which makes the map easier to construct)
+	Data map[string]interface{}
+	// VerifySSL is a flag that specifies if we should validate the server's TLS certificate. It should be noted that
+	// Go's TLS verify mechanism doesn't validate if a certificate has been revoked
+	VerifyTLS bool
+	UserAgent string
+}
+
+type Response struct {
+	// Ok is a boolean flag that validates that the server returned a 2xx code
+	Ok bool
+	// This is the Go error flag – if something went wrong within the request, this flag will be set.
+	Error error
+	// We want to abstract (at least at the moment) the Go http.Response object away. So we are going to make use of it
+	// internal but not give the user access
+	resp *http.Response
+}
+
+func buildResponse(resp *http.Response, err error) *Response {
+	// If the connection didn't succeed we just return a blank response
+	if err != nil {
+		return &Response{Error: err}
+	}
+
+	resp.Close = true // This will close the connection
+
+	return &Response{
+		Ok:    resp.StatusCode <= 200 && resp.StatusCode > 300,
+		Error: nil,
+		resp:  resp,
+	}
+}
+
+func (r *Response) JSON(userStruct interface{}) error {
+	jsonDecoder := json.NewDecoder(r.resp.Body)
+	defer r.resp.Body.Close()
+
+	if err := jsonDecoder.Decode(&userStruct); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}
+
+// GET takes 2 parameters and returns a Response channel. These two options are:
+// 1. A URL
+// 2. A RequestOptions struct
+func Get(url string, ro *RequestOptions) chan *Response {
+	responseChan := make(chan *Response)
+	go func() {
+		if ro == nil {
+			responseChan <- buildResponse(http.Get(url))
+		}
+
+	}()
+	return responseChan
+}
+
+func Put(url string, ro *RequestOptions) chan *Response     { return nil }
+func Post(url string, ro *RequestOptions) chan *Response    { return nil }
+func Delete(url string, ro *RequestOptions) chan *Response  { return nil }
+func Head(url string, ro *RequestOptions) chan *Response    { return nil }
+func Options(url string, ro *RequestOptions) chan *Response { return nil }
